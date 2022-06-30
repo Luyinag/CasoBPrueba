67f95af541e751a9b84d31d7fc942187b042f438
Luyinag <85760477+Luyinag@users.noreply.github.com>
2022-06-29 22:14:52 -0400

    Add files via upload

---
+++ Proyectos/Proyectos/Plankton/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+# Create your models here.
+
+#Modelo Usuarios
+class Usuario(models.Model):
+    run=models.IntegerField(primary_key=True, verbose_name="Rut")
+    correo=models.CharField(max_length=50, verbose_name="Correo")
+    contrasena=models.CharField(max_length=20, verbose_name="Contraseña")
+    nombre=models.CharField(max_length=20, verbose_name="Nombre")
+    apaterno=models.CharField(max_length=20, verbose_name="Apellido paterno")
+    amaterno=models.CharField(max_length=20, verbose_name="Apellido materno")
+    celular=models.IntegerField(verbose_name="Celular")
+
+    def __str__(self):
+        return self.run
+
+class Categoria(models.Model):
+    idCategoria=models.AutoField(primary_key=True, verbose_name="ID Categoria")
+    nombreCategoria=models.CharField(max_length=50,verbose_name="Nombre Categoria")
+
+    def __str__(self):
+        return self.nombreCategoria
+
+class Producto(models.Model):
+    idProducto=models.AutoField(primary_key=True,verbose_name="ID Producto")
+    nombre=models.CharField(max_length=20,verbose_name="Nombre")
+    descripcion=models.CharField(max_length=200, verbose_name="Descripción")
+    precio=models.IntegerField(verbose_name="Precio")
+    descuento=models.IntegerField(verbose_name="Porcentaje de oferta", default=0)
+    foto=models.ImageField(upload_to='users/%Y/%m/%d/', height_field=None, width_field=None, max_length=100, verbose_name="Foto")
+    destacado=models.BooleanField(verbose_name="Producto destacado")
+    categoria=models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nombre
+
